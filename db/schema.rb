921bb935ca36bdef2154e685c63a3fe2a13b62fd
yianna <yiannakokalas@gmail.com>
2017-08-12 14:33:09 -0400

    run migrations and generate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728185415) do
+ActiveRecord::Schema.define(version: 20170812131023) do
 
   create_table "current_locations", force: :cascade do |t|
     t.float "latitude"
     t.float "longitude"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
