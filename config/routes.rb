16f7e94692a552fac25e236c9334b1ed8e59e47d
yianna <yiannakokalas@gmail.com>
2017-08-12 14:33:47 -0400

    Add Sessions Login and Users Signup
    - Add two post routes
    - one for sessions#login
    - another for users#signup for registration and login.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/search', to: 'beauty_places#index'
   get '/details', to: 'beauty_places#details'
   get '/current_location', to: 'current_location#show'
+  post '/login', to: 'sessions#login'
+  post '/signup', to: 'users#signup'
 end
